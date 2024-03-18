1443d4bb340421ad9bbab2173159a504ab733be0
Roger Simanjuntak <arbination13@gmail.com>
2024-03-18 08:36:22 +0700

    responsive

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -8,6 +8,112 @@ class DetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailWebWidget(place: place);
+        } else {
+          return DetailMobileWidget(place: place);
+        }
+      },
+    );
+  }
+}
+
+class DetailWebWidget extends StatelessWidget {
+  final RestaurantPlace place;
+  const DetailWebWidget({Key? key, required this.place}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 16,
+        horizontal: 64,
+      ),
+      child: Center(
+        child: SizedBox(
+          width: screenWidth <= 1200 ? 800 : 1200,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ClipRRect(
+                  child: Container(
+                    width: 500,
+                    child: Image.asset(
+                      place.imgAsset,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              SizedBox(
+                width: 32,
+              ),
+              Expanded(
+                  child: Card(
+                child: Container(
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 20),
+                        child: Text(
+                          place.name,
+                          style: GoogleFonts.poppins(
+                              fontSize: 30.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      DetailListWidget(place: place),
+                      Container(
+                          margin: EdgeInsets.only(top: 30),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Description',
+                                style: GoogleFonts.poppins(
+                                    fontWeight: FontWeight.w700, fontSize: 18),
+                              ),
+                              Container(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 16.0),
+                                child: Text(
+                                  place.description,
+                                  style: GoogleFonts.poppins(fontSize: 16.0),
+                                  textAlign: TextAlign.justify,
+                                ),
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
+                ),
+              ))
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
+
+class DetailMobileWidget extends StatelessWidget {
+  const DetailMobileWidget({
+    super.key,
+    required this.place,
+  });
+
+  final RestaurantPlace place;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
           child: Column(
@@ -81,78 +187,7 @@ class DetailScreen extends StatelessWidget {
             ),
           ),
           const SizedBox(height: 15),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15),
-            padding: const EdgeInsets.all(15),
-            decoration: BoxDecoration(
-                border: Border.all(width: 2.1, color: Colors.black54),
-                borderRadius: BorderRadius.circular(12),
-                color: Colors.white,
-                boxShadow: const [
-                  BoxShadow(
-                      color: Colors.black26,
-                      offset: Offset(15, 15),
-                      blurRadius: 20,
-                      blurStyle: BlurStyle.normal),
-                ]),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    const Icon(
-                      Icons.calendar_month,
-                      size: 30,
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      place.open,
-                      style: GoogleFonts.poppins(
-                          fontSize: 14, fontWeight: FontWeight.w600),
-                    )
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  height: 50,
-                  decoration: BoxDecoration(border: Border.all(width: 1)),
-                ),
-                Column(
-                  children: [
-                    const Icon(
-                      Icons.timer_outlined,
-                      size: 30,
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      place.time,
-                      style: GoogleFonts.poppins(
-                          fontSize: 14, fontWeight: FontWeight.w600),
-                    )
-                  ],
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  height: 50,
-                  decoration: BoxDecoration(border: Border.all(width: 1)),
-                ),
-                Column(
-                  children: [
-                    const Icon(
-                      Icons.pin_drop_outlined,
-                      size: 30,
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      place.address,
-                      style: GoogleFonts.poppins(
-                          fontSize: 14, fontWeight: FontWeight.w600),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          ),
+          DetailListWidget(place: place),
           Container(
               padding: const EdgeInsets.all(15),
               margin: const EdgeInsets.only(top: 10),
@@ -180,6 +215,117 @@ class DetailScreen extends StatelessWidget {
               ))
         ],
       )),
+    );
+  }
+}
+
+class DetailListWidget extends StatelessWidget {
+  const DetailListWidget({
+    super.key,
+    required this.place,
+  });
+
+  final RestaurantPlace place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 15),
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+          border: Border.all(width: 2.1, color: Colors.black54),
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black26,
+                offset: Offset(15, 15),
+                blurRadius: 20,
+                blurStyle: BlurStyle.normal),
+          ]),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              const Icon(
+                Icons.calendar_month,
+                size: 20,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                place.open,
+                style: GoogleFonts.poppins(
+                    fontSize: 14, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            height: 50,
+            decoration: BoxDecoration(border: Border.all(width: 1)),
+          ),
+          Column(
+            children: [
+              const Icon(
+                Icons.timer_outlined,
+                size: 20,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                place.time,
+                style: GoogleFonts.poppins(
+                    fontSize: 14, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            height: 50,
+            decoration: BoxDecoration(border: Border.all(width: 1)),
+          ),
+          Column(
+            children: [
+              const Icon(
+                Icons.pin_drop_outlined,
+                size: 20,
+              ),
+              const SizedBox(height: 5),
+              Text(
+                place.address,
+                style: GoogleFonts.poppins(
+                    fontSize: 14, fontWeight: FontWeight.w600),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FavoriteButton extends StatefulWidget {
+  const FavoriteButton({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteButtonState createState() => _FavoriteButtonState();
+}
+
+class _FavoriteButtonState extends State<FavoriteButton> {
+  bool isFavorite = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: Colors.red,
+      ),
+      onPressed: () {
+        setState(() {
+          isFavorite = !isFavorite;
+        });
+      },
     );
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -9,71 +9,147 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Home Screen',
-          style: GoogleFonts.poppins(),
+        appBar: AppBar(
+          title: Text(
+            'Home Screen ',
+            style: GoogleFonts.poppins(),
+          ),
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Colors.green,
+              Colors.yellow,
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+          ),
         ),
-        flexibleSpace: Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Colors.green,
-            Colors.yellow,
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-        ),
-      ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final RestaurantPlace place = RestaurantPlaceList[index];
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth <= 600) {
+              return ListViewPhone();
+            } else if (constraints.maxWidth <= 1200) {
+              return GridViewDesk(
+                gridCount: 3,
+              );
+            } else {
+              return GridViewDesk(
+                gridCount: 6,
+              );
+            }
+          },
+        ));
+  }
+}
+
+class ListViewPhone extends StatelessWidget {
+  const ListViewPhone({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        final RestaurantPlace place = RestaurantPlaceList[index];
+        return InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) {
+                return DetailScreen(
+                  place: place,
+                );
+              },
+            ));
+          },
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                      height: 100,
+                      width: 250,
+                      child: Image.asset(
+                        place.imgAsset,
+                        fit: BoxFit.fill,
+                      )),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          place.name,
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(place.address),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+      itemCount: RestaurantPlaceList.length,
+    );
+  }
+}
+
+class GridViewDesk extends StatelessWidget {
+  final int gridCount;
+  const GridViewDesk({Key? key, required this.gridCount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: gridCount,
+        children: RestaurantPlaceList.map((place) {
           return InkWell(
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) {
-                  return DetailScreen(
-                    place: place,
-                  );
-                },
-              ));
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailScreen(place: place);
+              }));
             },
             child: Card(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
                   Expanded(
-                    flex: 1,
-                    child: Container(
-                        height: 100,
-                        width: 250,
-                        child: Image.asset(
-                          place.imgAsset,
-                          fit: BoxFit.fill,
-                        )),
+                    child: Image.asset(
+                      place.imgAsset,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            place.name,
-                            style: const TextStyle(fontSize: 16.0),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(place.address),
-                        ],
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      place.name,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, bottom: 8.0),
+                    child: Text(
+                      place.address,
+                    ),
+                  ),
                 ],
               ),
             ),
           );
-        },
-        itemCount: RestaurantPlaceList.length,
+        }).toList(),
       ),
     );
   }
